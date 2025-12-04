9b6eba2d3f75bc8ecb6e3c2cf00ca0742cc8ef62
Pascal Strebel <pas3398@hotmail.ch>
2025-12-04 08:22:15 +0100

    forklift certification

--- day04/code.py
+++ day04/code.py
@@ -8,16 +8,6 @@ def read_input(input_file_name):
         lines = f.readlines()
         return lines
 
-
-class Dummy:
-    def __init__(self):
-        self.dummy = 0
-
-    def setup(self, input):
-        for set in re.findall(r"[^;]+", input):
-            pass
-
-
 # --- Day 4: Printing Department ---
 
 # You ride the escalator down to the printing department. They're clearly 
@@ -76,16 +66,182 @@ class Dummy:
 # Consider your complete diagram of the paper roll locations. How many rolls 
 # of paper can be accessed by a forklift?
 
+def count_neighbor_rolls(map, i, j):
+    neighborhood = [row[max(j-1,0):min(len(row),j+2)] for row in map[max(0,i-1):min(len(map),i+2)]]
+    return sum(sum(cell.count('@') for cell in row) for row in neighborhood) - 1 if map[i][j] == '@' else 0
+
 def compute_part_1(input_file_name="input.txt"):
     input = read_input(input_file_name)
-    numbers = [[int(x) for x in re.findall(r"\d+", line)] for line in input]
-    return 0
+    map = [[x for x in re.findall(r"\.|\@", line)] for line in input]
+    count = 0
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            count += map[i][j] == '@' and count_neighbor_rolls(map, i, j) < 4
+    return count
 
+# 1376
+# That's the right answer! You are one gold star closer to decorating the 
+# North Pole.
+
+# --- Part Two ---
+
+# Now, the Elves just need help accessing as much of the paper as they can.
+
+# Once a roll of paper can be accessed by a forklift, it can be removed. Once 
+# a roll of paper is removed, the forklifts might be able to access more 
+# rolls of paper, which they might also be able to remove. How many total 
+# rolls of paper could the Elves remove if they keep repeating this process?
+
+# Starting with the same example as above, here is one way you could remove 
+# as many rolls of paper as possible, using highlighted @ to indicate that a 
+# roll of paper is about to be removed, and using x to indicate that a roll 
+# of paper was just removed:
+
+# Initial state:
+# ..@@.@@@@.
+# @@@.@.@.@@
+# @@@@@.@.@@
+# @.@@@@..@.
+# @@.@@@@.@@
+# .@@@@@@@.@
+# .@.@.@.@@@
+# @.@@@.@@@@
+# .@@@@@@@@.
+# @.@.@@@.@.
+
+# Remove 13 rolls of paper:
+# ..xx.xx@x.
+# x@@.@.@.@@
+# @@@@@.x.@@
+# @.@@@@..@.
+# x@.@@@@.@x
+# .@@@@@@@.@
+# .@.@.@.@@@
+# x.@@@.@@@@
+# .@@@@@@@@.
+# x.x.@@@.x.
+
+# Remove 12 rolls of paper:
+# .......x..
+# .@@.x.x.@x
+# x@@@@...@@
+# x.@@@@..x.
+# .@.@@@@.x.
+# .x@@@@@@.x
+# .x.@.@.@@@
+# ..@@@.@@@@
+# .x@@@@@@@.
+# ....@@@...
+
+# Remove 7 rolls of paper:
+# ..........
+# .x@.....x.
+# .@@@@...xx
+# ..@@@@....
+# .x.@@@@...
+# ..@@@@@@..
+# ...@.@.@@x
+# ..@@@.@@@@
+# ..x@@@@@@.
+# ....@@@...
+
+# Remove 5 rolls of paper:
+# ..........
+# ..x.......
+# .x@@@.....
+# ..@@@@....
+# ...@@@@...
+# ..x@@@@@..
+# ...@.@.@@.
+# ..x@@.@@@x
+# ...@@@@@@.
+# ....@@@...
+
+# Remove 2 rolls of paper:
+# ..........
+# ..........
+# ..x@@.....
+# ..@@@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@x.
+# ....@@@...
+
+# Remove 1 roll of paper:
+# ..........
+# ..........
+# ...@@.....
+# ..x@@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@..
+# ....@@@...
+
+# Remove 1 roll of paper:
+# ..........
+# ..........
+# ...x@.....
+# ...@@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@..
+# ....@@@...
+
+# Remove 1 roll of paper:
+# ..........
+# ..........
+# ....x.....
+# ...@@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@..
+# ....@@@...
+
+# Remove 1 roll of paper:
+# ..........
+# ..........
+# ..........
+# ...x@@....
+# ...@@@@...
+# ...@@@@@..
+# ...@.@.@@.
+# ...@@.@@@.
+# ...@@@@@..
+# ....@@@...
+
+# Stop once no more rolls of paper are accessible by a forklift. In this 
+# example, a total of 43 rolls of paper can be removed.
+
+# Start with your original diagram. How many rolls of paper in total can be 
+# removed by the Elves and their forklifts?
 
 def compute_part_2(input_file_name="input.txt"):
     input = read_input(input_file_name)
-    return 0
+    map = [[x for x in re.findall(r"\.|\@", line)] for line in input]
+    total = 0
+    while True:
+        count = 0
+        for i in range(len(map)):
+            for j in range(len(map[i])):
+                if map[i][j] == '@' and count_neighbor_rolls(map, i, j) < 4:
+                    count += 1
+                    map[i][j] = '.'
+        if count == 0:
+            break
+        total += count
+    return total
 
+# 8587
+# That's the right answer! You are one gold star closer to decorating the 
+# North Pole.
 
 if __name__ == "__main__":
     print(f"PART 1: {compute_part_1()}")
